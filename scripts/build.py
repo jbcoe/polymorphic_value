491a4887762956e045b6506611a474a86a1d3484
Jonathan B. Coe <jbcoe@me.com>
2016-11-15 23:07:57 +0000

    Format build.py using VS Code

--- scripts/build.py
+++ scripts/build.py
@@ -3,25 +3,40 @@ import sys
 import os
 import subprocess
 
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", help="run tests", action="store_true", dest="run_tests")
-    parser.add_argument("-o", help="output dir (relative to source dir)", default="build", dest="out_dir")
-    parser.add_argument("-c", help="config (Debug or Release)", default="Release", dest="config")
+    parser.add_argument(
+        "-t", help="run tests", action="store_true", dest="run_tests")
+    parser.add_argument(
+        "-o",
+        help="output dir (relative to source dir)",
+        default="build",
+        dest="out_dir")
+    parser.add_argument(
+        "-c",
+        help="config (Debug or Release)",
+        default="Release",
+        dest="config")
     args = parser.parse_args()
-    
+
     src_dir = os.path.dirname(os.path.dirname(__file__))
-    
-    subprocess.check_call("cmake . -B{} -DCMAKE_BUILD_TYPE={}".format(args.out_dir, args.config).split(), cwd=src_dir)
-    subprocess.check_call("cmake --build ./{}".format(args.out_dir).split(), cwd=src_dir)
-    
-    
+
+    subprocess.check_call(
+        "cmake . -B{} -DCMAKE_BUILD_TYPE={}".format(args.out_dir,
+                                                    args.config).split(),
+        cwd=src_dir)
+    subprocess.check_call(
+        "cmake --build ./{}".format(args.out_dir).split(), cwd=src_dir)
+
     if args.run_tests:
-        rc = subprocess.call("ctest . --output-on-failure -c {}".format(args.config).split(), cwd=os.path.join(src_dir,args.out_dir))
+        rc = subprocess.call(
+            "ctest . --output-on-failure -c {}".format(args.config).split(),
+            cwd=os.path.join(src_dir, args.out_dir))
         if rc != 0:
             sys.exit(1)
 
+
 if __name__ == "__main__":
     main()
-
