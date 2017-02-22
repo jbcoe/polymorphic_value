1f9d5f7962f7d6816c32a62a754f1fa1b0c52079
Jonathan B Coe <jbcoe@users.noreply.github.com>
2017-02-22 19:34:03 +0000

    correct exception safety-guarantee of pointer constructor (#23)
    * correct exception safety-guarantee of pointer constructor
    
    closes #21
    
    * fix merge issue

--- polymorphic_value.h
+++ polymorphic_value.h
@@ -68,6 +68,11 @@ public:
   {
   }
 
+  explicit pointer_control_block(std::unique_ptr<U, D> p, C c = C{})
+      : C(std::move(c)), p_(std::move(p))
+  {
+  }
+
   std::unique_ptr<control_block<T>> clone() const override
   {
     assert(p_);
@@ -158,9 +163,10 @@ public:
     }
 
     assert(typeid(*u) == typeid(U));
+    std::unique_ptr<U, D> p(u, std::move(deleter));
 
     cb_ = std::make_unique<pointer_control_block<T, U, C, D>>(
-        u, std::move(copier), std::move(deleter));
+        std::move(p), std::move(copier));
     ptr_ = u;
   }
 
