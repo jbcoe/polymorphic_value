17276bc169b8fcfe9a55941d3c3910985973ff5d
Jonathan B. Coe <jonathanbcoe@gmail.com>
2017-11-20 09:55:51 +0000

    [wip] adding boost tests for deep copy

--- test/boost_test_polymorphic_value.cpp
+++ test/boost_test_polymorphic_value.cpp
@@ -7,7 +7,17 @@
 
 using boost::polymorphic_value;
 
-class Shape {};
+struct Shape {
+  virtual double area() const = 0;
+};
+
+class Square : public Shape {
+  double side_;
+
+public:
+  Square(double side) : side_(side) {}
+  double area() const override { return side_ * side_; }
+};
 
 BOOST_AUTO_TEST_CASE(empty_upon_default_construction) {
   polymorphic_value<Shape> pv;
@@ -15,3 +25,41 @@ BOOST_AUTO_TEST_CASE(empty_upon_default_construction) {
   BOOST_TEST(!bool(pv));
 }
 
+BOOST_AUTO_TEST_CASE(non_empty_upon_value_construction) {
+  polymorphic_value<Square> pv(Square(2));
+
+  BOOST_TEST(bool(pv));
+}
+
+BOOST_AUTO_TEST_CASE(pointer_like_methods_access_owned_object) {
+  polymorphic_value<Shape> pv(Square(2));
+
+  BOOST_TEST(pv->area() == 4);
+}
+
+BOOST_AUTO_TEST_CASE(const_is_propagated) {
+  polymorphic_value<Shape> pv(Square(2));
+  static_assert(std::is_same<Shape*, decltype(pv.operator->())>::value, "");
+  static_assert(std::is_same<Shape&, decltype(pv.operator*())>::value, "");
+
+  const polymorphic_value<Shape> cpv(Square(2));
+  static_assert(std::is_same<const Shape*, decltype(cpv.operator->())>::value,
+                "");
+  static_assert(std::is_same<const Shape&, decltype(cpv.operator*())>::value,
+                "");
+}
+
+BOOST_AUTO_TEST_CASE(copies_are_deep) {
+  polymorphic_value<Shape> pv(Square(2));
+  auto pv2 = pv;
+
+  //BOOST_TEST(pv.operator*() != pv2.operator*());
+}
+
+BOOST_AUTO_TEST_CASE(assigned_copies_are_deep) {
+  polymorphic_value<Shape> pv(Square(2));
+  polymorphic_value<Shape> pv2;
+  pv2 = pv;
+
+  //BOOST_TEST(pv.operator*() != pv2.operator*());
+}
