6dd556770130dbc0df5cdb3d77af1a5c3e715583
Jonathan B. Coe <jonathanbcoe@gmail.com>
2017-12-08 08:40:07 +0000

    fix typo and add Google Drive export

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -u
 set -e
-BOOST_ROOT=/Users/jon/DEV/boost_1_65_1/ ../../boost_1_65_1/b2
+BOOST_ROOT=/Users/jon/DEV/boost_1_65_1/ ../../boost_1_65_1/b2 && 
+  cp -R html/ ~/Google\ Drive/boost/boost-polymorphic-value
