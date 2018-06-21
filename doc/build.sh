8b545f4415114dc787de96425fc3c0320fe41e26
Jonathan B. Coe <jonathanbcoe@gmail.com>
2018-06-21 09:06:53 +0100

    Added script to push docs

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-set -u
-set -e
-(export BOOST_ROOT=/Users/jon/DEV/boost_1_65_1/; $BOOST_ROOT/b2)
+set -eu
+
+# Build the docs
+(cd $(dirname $0); export BOOST_ROOT=/Users/jon/DEV/boost_1_65_1/; "$BOOST_ROOT"/b2)
+
