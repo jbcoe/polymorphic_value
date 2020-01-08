9204db70b3becf580615b705f5cf56d1e82f9078
Antony Peacock <ant.peacock@gmail.com>
2020-01-08 10:53:23 +0000

    Load version from CMakelist.txtm, and do not duplicate

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from conans import ConanFile, CMake
+from conans.tools import load
+import re, os
 
 class PolymorphicValueConan(ConanFile):
     name = "polymorphic_value"
-    version = "1.3.0"
     license = "MIT"
     url = "https://github.com/jbcoe/polymorphic_value"
     description = "A polymorphic value type proposed for inclusion to the C++ 20 Standard Library"
@@ -12,9 +13,14 @@ class PolymorphicValueConan(ConanFile):
     exports_sources = "*.txt", "*.h", "*.natvis", "*.cpp", "*.cmake", "*.cmake.in", "LICENSE.txt"
     generators = "cmake"
 
+    def set_version(self):
+        content = load(os.path.join(os.path.dirname(__file__), "CMakeLists.txt"))
+        version = re.search(r"set\(POLYMOPHIC_VALUE_VERSION (.*)\)", content).group(1)
+        self.version = version.strip()
+
     _cmake = None
     @property
-    def cmake(self):
+    def cmake(self):git
         if self._cmake is None:
             self._cmake = CMake(self)
             self._cmake.definitions.update({
