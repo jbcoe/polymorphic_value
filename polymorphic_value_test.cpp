fae4c74377b2ddd623c0230acb8da7d92387ceec
Jonathan B Coe <jonathanbcoe@gmail.com>
2022-07-09 00:55:00 +0100

    Replace Jonathan Coe with Indirect Value Authors in licenses

--- polymorphic_value_test.cpp
+++ polymorphic_value_test.cpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2016 Jonathan B. Coe
+Copyright (c) 2016 The Polymorphic Value Authors. All Rights Reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
